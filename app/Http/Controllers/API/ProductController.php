4e991f5f127898c06af51ae88dd01cbe36753d6d
maala <maala.mhrez.it@hotmail.com>
2019-02-09 04:33:20 +0200

    delete unnecessary files and comments

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -9,22 +9,11 @@ use App\Price_group ;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return response()->json(Product::all()) ;
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $product = new Product ;
@@ -40,25 +29,12 @@ class ProductController extends Controller
         return response()->json([$res]) ;
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $product = Product::find($id) ;
         return response()->json([$product]) ;
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         $newName = $request->input('newName') ;
@@ -68,12 +44,6 @@ class ProductController extends Controller
         return response()->json([$product]) ;
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $product = Product::where('id',$id)->delete() ;
